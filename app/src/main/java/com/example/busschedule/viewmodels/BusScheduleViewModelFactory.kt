ccd7c50b0c477cad0d150a238b9e1cca679a0c54
Sourabh Kumar <20010001030@gateway.edu.in>
2022-08-26 00:05:55 +0530

    Created ViewModel and factory class

---
+++ app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModelFactory.kt
@@ -0,0 +1,22 @@
+package com.example.busschedule.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.busschedule.database.schedule.ScheduleDao
+
+//If you instantiate it directly in one of your fragments, then your fragment object will have
+// to handle everything, including all the memory management
+//so that your view model can be lifecycle aware without your fragment having to handle
+// this directly.
+class BusScheduleViewModelFactory(
+    private val scheduleDao: ScheduleDao
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return BusScheduleViewModel(scheduleDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
+}
