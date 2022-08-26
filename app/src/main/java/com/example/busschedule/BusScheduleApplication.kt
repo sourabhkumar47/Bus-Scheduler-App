05a01e80e03d145fd45823ea12d1524a7fa6afda
Sourabh Kumar <20010001030@gateway.edu.in>
2022-08-26 19:01:27 +0530

    Created BusScheduleApplication.kt

--- app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
+++ app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
@@ -1,2 +1,8 @@
 package com.example.busschedule
 
+import android.app.Application
+import database.AppDatabase
+
+class BusScheduleApplication : Application() {
+    val database: AppDatabase by lazy { AppDatabase.getDatabase(this)}
+}
