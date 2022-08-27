48817122ce346db8833d987a5b41149249f5aff9
Sourabh Kumar <20010001030@gateway.edu.in>
2022-08-27 18:46:39 +0530

    Fixed error

--- app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
+++ app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
@@ -1,7 +1,7 @@
 package com.example.busschedule
 
 import android.app.Application
-import database.AppDatabase
+import com.example.busschedule.database.AppDatabase
 
 class BusScheduleApplication : Application() {
     val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
